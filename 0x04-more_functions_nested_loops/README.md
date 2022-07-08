3d2dac88ad90581add42ad76a25e34ab5e1f4e71
Biruck <biruck3@gmail.com>
2022-07-08 05:05:06 -0700

    function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Task for 0x04. C - More functions, more nested loops 
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+/** 
+ * Description: header file containing prototype of all function used in 0x04. C * - More functions, more nested loops directory
+ */
+int _putchar(char c);
+int _isupper(int c);
+
