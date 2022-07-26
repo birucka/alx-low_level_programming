7fa111dbbadb956fda59965e151bac2ebfa6cf23
Biruck <biruck3@gmail.com>
2022-07-26 12:43:53 -0700

    corrected task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,31 +2,31 @@
 #include "main.h"
 
 /**
- * *_strdup - copies the string given as parameter
+ *_strdup - copies the string given as parameter
  * @str: string to duplicate
  * Return: pointer to the copied string (Success), NULL (Error)
  */
 char *_strdup(char *str)
 {
-	char *dup;
-	unsigned int i, len;
-
-	i = 0;
-	len = 0;
+	char *duplicate;
+	int index, len = 0;
 
 	if (str == NULL)
-		return (NULL)
-
-			while (str[len])
-				len++;
-
-	dup = malloc(sizeof(char) * (len + 1));
-
-	if (dup == NULL)
 		return (NULL);
 
-	while ((dup[i] = str[i]) != '\0')
-		i++;
+	for (index = 0; str[index]; index++)
+		len++;
 
-	return (dup);
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
+	duplicate[len] = '\0';
+
+	return (duplicate);
 }
+
