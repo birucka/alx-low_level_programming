8dae40833ce2d60ffd47922ecc7c44da3e366382
Biruck <biruck3@gmail.com>
2022-08-13 01:07:30 -0700

    all fix errors

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -14,7 +14,7 @@ int op_mod(int a, int b);
  */
 int op_add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
 
 /**
@@ -25,7 +25,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	return(a - b);
+	return (a - b);
 }
 
 /**
