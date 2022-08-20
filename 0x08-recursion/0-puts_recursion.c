135b89d0281eec696cef98c584702ea480f6e13f
Biruck <biruck3@gmail.com>
2022-08-20 05:47:07 -0700

    first task 0 done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion- functions that prints a string
+ * @s: the strings that will be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+		_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
+else
+_putchar('\n');
+}
+
